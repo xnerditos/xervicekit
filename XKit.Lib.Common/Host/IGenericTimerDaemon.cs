738362b02930757f023ce660a8955b419d532b02
Paul Jonathan Zoulin <toochevere>
2022-07-27 19:12:00 +0000

    Daemon improvements
    Expose SetTimerDelay on ServiceDaemon
    Guarantee that the timer event always fires if OnDetermineTimerEventPeriod is not provided.
    Refactor out some complexity

--- XKit.Lib.Common/Host/IGenericTimerDaemon.cs
+++ XKit.Lib.Common/Host/IGenericTimerDaemon.cs
@@ -3,7 +3,7 @@ namespace XKit.Lib.Common.Host {
     public interface IGenericTimerDaemonOperation : IServiceDaemonOperation<object> { }
 
     public interface IGenericTimerDaemon : IServiceDaemon<object> { 
-        void SetTimerDelay(uint? milliseconds);
+        void SetTimerInterval(uint? milliseconds);
         void SetTimerEnabled(bool enabled);
     } 
 }
